1596dc08a6332aecdf7d7e4923f3c08b640f7eba
Pratiksha <singhpratiksha450@gmail.com>
2025-05-25 15:25:56 +0530

    UI DONE

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+// import footerLogo from "../../assets/logo.png";
+// import Banner from "../../assets/website/footer-pattern.jpg";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin
+} from "react-icons/fa";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { FiShoppingBag } from "react-icons/fi";
+import { IoCall } from "react-icons/io5";
+
+// const BannerImg = {
+//   backgroundImage: `url(${Banner})`,
+//   backgroundPosition: "bottom",
+//   backgroundRepeat: "no-repeat",
+//   backgroundSize: "cover",
+//   height: "100%",
+//   width: "100%",
+// };
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className="text-white bg-[#212529]">
+      <div className="container">
+        <div data-aos="zoom-in" className="grid md:grid-cols-3 pb-44 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-1">
+              <FiShoppingBag size="30" />
+              {/* <img src={footerLogo} alt="" className="max-w-[50px]" /> */}
+              Let's Shop
+            </h1>
+            <p>
+              Discover the joy of shopping with us! From the latest trends to everyday 
+              essentials, we’ve got everything you need at unbeatable prices. Shop with 
+              confidence, enjoy secure payments, and fast delivery to your doorstep. Follow us on social media for 
+              exclusive deals and updates. Your style, your choice – made easy!
+            </p>
+          </div>
+
+          {/* Footer Links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Important Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* social links */}
+
+            <div>
+              <div className="flex items-center gap-3 mt-6">
+                <a href="#">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3">
+                  <FaMapLocationDot />
+                  <p>Roorkee, India</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <IoCall />
+                  <p>+91 123456789</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Copyright Section */}
+      <div className="bg-[#181A1D] py-4 text-center">
+        <p className="text-gray-400 text-sm">
+          © 2024 Let's Shop. All Rights Reserved. Designed with ❤️ by Pratiksha Singh
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
